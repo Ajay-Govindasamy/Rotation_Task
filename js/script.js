9e7eb0c19ee2e3df2115f607daff0dfe06c56a34
Ajay Govindasamy <ajaygovindan1995@gmail.com>
2021-07-02 11:18:13 +0100

    form validations

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function formSubmit(event) {
 
   const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
-  const arr = new Uint8ClampedArray(40000);
+  const arr = new Uint8ClampedArray(formData.get('buffer'));
 
   // Iterate through every pixel
   for (let i = 0; i < arr.length; i += 4) {
@@ -19,4 +19,36 @@ function formSubmit(event) {
 
   // Draw image data to the canvas
   ctx.putImageData(imageData, 20, 20);
+}
+
+//validating Input Fields
+function validateField(fieldObject) {
+  switch (fieldObject.name) {
+    case 'buffer':
+      Math.sign(fieldObject.value) == 1
+        ? (document.getElementById('bufferError').innerHTML = '')
+        : (document.getElementById('bufferError').innerHTML =
+            'Enter positive buffer value');
+      break;
+    case 'width':
+      Math.sign(fieldObject.value) == 1
+        ? (document.getElementById('widthError').innerHTML = '')
+        : (document.getElementById('widthError').innerHTML =
+            'Enter positive width value');
+      break;
+    case 'height':
+      Math.sign(fieldObject.value) == 1
+        ? (document.getElementById('heightError').innerHTML = '')
+        : (document.getElementById('heightError').innerHTML =
+            'Enter positive height value');
+      break;
+    case 'angle':
+      fieldObject.value > 0 && fieldObject.value <= 360
+        ? (document.getElementById('angleError').innerHTML = '')
+        : (document.getElementById('angleError').innerHTML =
+            'Enter value between 1 and 360');
+      break;
+    default:
+      break;
+  }
 }
